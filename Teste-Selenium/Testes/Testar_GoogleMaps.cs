8919f071d773f2b59fabe4357ca03c3e66c64de1
tmendes <th.mendes1997@gmail.com>
2019-04-26 18:53:18 -0300

    Inicio

---
+++ Teste-Selenium/Testes/Testar_GoogleMaps.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Teste_Geral.PageObjects;
+
+namespace Teste_GoogleMaps
+{
+    [TestFixture]
+    public class TestarGoogleMaps
+    {
+        GoogleMaps x = new GoogleMaps();
+
+        [SetUp]
+        public void SetupTest()
+        {
+            x.homeURL = "https://www.google.com.br/maps";
+            x.webDriver = new ChromeDriver();
+        }
+
+        [TestCase("Praça da Sé", "Masp")]
+        [TestCase("Parque Ibirapuera", "Metrô Ana Rosa")]
+        [TestCase("Viaduto do Chá", "Enseada Guarujá")]
+        public void TesteGoogleMaps(string local_desejado, string local_atual)
+        {
+            x.homeURL = "https://www.google.com.br/maps";
+            x.webDriver.Navigate().GoToUrl(x.homeURL);
+            x.Buscar(x.webDriver, local_desejado);
+            x.SelectRota(x.webDriver);
+            x.DefinirLocalPartida(x.webDriver, local_atual);
+
+        }
+
+        public void TearDownTest()
+        {
+            x.webDriver.Close();
+        }
+
+
+    }
+
+
+}
+
---
+++ Teste-Selenium/Utils/Utils.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace Utils
+{
+    public static class Utilitarios
+    {
+
+        public static void SetText(this IWebDriver webDriver,
+            By by, string text)
+        {
+            IWebElement webElement = webDriver.FindElement(by);
+            webElement.SendKeys(text);
+        }
+
+        public static void Submit(this IWebDriver webDriver, By by)
+        {
+            IWebElement webElement = webDriver.FindElement(by);
+            webElement.Submit();
+        }
+
+    }
+}
